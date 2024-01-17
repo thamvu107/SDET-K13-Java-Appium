4972791bf92e64a7d8ff3a383ae8a55083984731
Tham <thamvu107@gmail.com>
2024-01-17 21:40:31 +0700

    lesson 3: looping

---
+++ src/lessons/lesson3/Array.java
@@ -0,0 +1,12 @@
+package src.lessons.lesson3;
+
+public class Array {
+    public static void main(String[] args) {
+        int start = 0;
+
+        while (start < 5) {
+            System.out.println("Loop is continue");
+            start++;
+        }
+    }
+}
---
+++ src/lessons/lesson3/LoopFlowControlKeywords.java
@@ -0,0 +1,50 @@
+package src.lessons.lesson3;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LoopFlowControlKeywords {
+    public static void main(String[] args) {
+        /*
+         * Problem:
+         * Given an array of random integer numbers in range of 100 with 10 elements.
+         * Print out the first index of the target number.
+         * Allow user to input the target number
+         * */
+
+        final int ARRAY_LENGTH = 10;
+        final int MAX_VALUE = 10;
+        int[] myArray = new int[ARRAY_LENGTH];
+
+        for (int index = 0; index < ARRAY_LENGTH; index++) {
+            int randomValue = new SecureRandom().nextInt(MAX_VALUE + 1);
+            myArray[index] = randomValue;
+        }
+
+        System.out.println(Arrays.toString(myArray));
+        System.out.println("Please enter your target number: ");
+        Scanner input = new Scanner(System.in);
+        int targetNumber = input.nextInt();
+        boolean found = false;
+
+        /*
+         * loop over the array
+         *   if element == targetNumber
+         *       printout the index
+         *       exit the loop
+         * */
+
+        for (int index = 0; index < myArray.length; index++) {
+            if (myArray[index] == targetNumber) {
+                found = true;
+                System.out.printf(String.format("The number %d is found at the index %d", targetNumber, index));
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.printf(String.format("The number %d is not found", targetNumber));
+        }
+    }
+}
