2ff1826945278376a127b25b00d3d351697df748
Tham <thamvu107@gmail.com>
2024-02-22 12:07:46 +0700

    lesson 8

--- src/lessons/lesson_8/RobotCat.java
+++ src/lessons/lesson_8/RobotCat.java
@@ -1,2 +1,64 @@
-package src.lessons.lesson_8;public class RobotCat {
+package src.lessons.lesson_8;
+
+// Supper class, parent class
+public class RobotCat extends Machine{
+    private String name;
+    private String productionDate;
+
+    public RobotCat() {
+    }
+
+    public RobotCat(String name) {
+        this.name = name;
+    }
+
+    public RobotCat(String name, String productionDate) {
+        this.name = name;
+        this.productionDate = productionDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProductionDate() {
+        return productionDate;
+    }
+
+    public void setProductionDate(String productionDate) {
+        this.productionDate = productionDate;
+    }
+
+    @Override
+    public void introduce() {
+        System.out.println("I'm RobotCat");
+    }
+
+    /*
+     * Overloading:
+     *  + SAME class
+     *  + Compile time
+     *  + Base on method signature: same function + different params
+     *
+     * */
+
+    /*
+    * Overriding happen when
+    * + at least 2 classes
+    * AND subclass class Inheritance parent class
+    * AND happen in running time
+    * method signature: same ( same function + same params)
+    * */
+    protected String charge(){
+        return "charging";
+    }
+    @Override
+    public String toString() {
+        return "RobotCat{" + "name='" + name + '\'' + "}\n";
+
+    }
 }
--- src/lessons/lesson_8/RobotCatTest.java
+++ src/lessons/lesson_8/RobotCatTest.java
@@ -1,2 +1,23 @@
-package src.lessons.lesson_8;public class RobotCatTest {
+package src.lessons.lesson_8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RobotCatTest {
+    public static void main(String[] args) {
+//        RobotCat batteryRobotCat = new BatteryRobotCat();
+//        batteryRobotCat.setName("BatteryRobotCat");
+//        System.out.println(batteryRobotCat.charge());
+//
+//        RobotCat solarRobotCat = new SolarRobotCat();
+//        batteryRobotCat.setName("SolarRobotCat");
+//        System.out.println(solarRobotCat.charge());
+
+        RobotCat batteryRobotCat = new BatteryRobotCat();
+        RobotCat solarRobotCat = new SolarRobotCat();
+        solarRobotCat.introduce();
+
+        List<RobotCat> robotCatList = Arrays.asList(batteryRobotCat, solarRobotCat);
+        RobotCatController.chargeRobots(robotCatList);
+    }
 }
