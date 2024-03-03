2a47e309b9e45d2a2e178a340209dc73268beb50
Tham <thamvu107@gmail.com>
2024-03-03 17:18:17 +0700

    Login Page

--- src/lessons/lesson_10/P04_LoginPage/ExternalLoginPage.java
+++ src/lessons/lesson_10/P04_LoginPage/ExternalLoginPage.java
@@ -1,5 +1,20 @@
 package src.lessons.lesson_10.P04_LoginPage;
 
-public class ExternalLoginPage {
+public class ExternalLoginPage extends LoginPage{
 
+
+    @Override
+    public void inputUsername(String username) {
+        System.out.println("External Username");
+    }
+
+    @Override
+    public void inputPassword(String password) {
+        System.out.println("External Password");
+    }
+
+    @Override
+    public void clickOnLoginBtn() {
+        System.out.println("External Clicked");
+    }
 }
--- src/lessons/lesson_10/P04_LoginPage/Test.java
+++
@@ -1,6 +0,0 @@
-package src.lessons.lesson_10.P04_LoginPage;
-
-public class Test {
-    LoginFlow flow = new LoginFlow("Teo", "124");
-
-}
