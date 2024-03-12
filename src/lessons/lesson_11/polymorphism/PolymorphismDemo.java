bc1f36518ae2655ccccea76fa7c7e26e4b9bf3e5
Tham <thamvu107@gmail.com>
2024-03-12 11:33:44 +0700

    Add a comment for learning purpose

--- src/lessons/lesson_11/polymorphism/PolymorphismDemo.java
+++ src/lessons/lesson_11/polymorphism/PolymorphismDemo.java
@@ -2,6 +2,7 @@ package src.lessons.lesson_11.polymorphism;
 
 public class PolymorphismDemo {
     public static void main(String[] args) {
+        // An object can trans into different form in the model
         Animal animal= new Animal();
         Animal pig = new Pig();
         Animal dog = new Dog();
